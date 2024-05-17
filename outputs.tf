66d23fc9c487bbaf6e611d06ba59c3889fdd3248
Vijay Viswanathan <varungur@redhat.com>
2024-05-17 11:31:32 -0500

    Updated output.tf file

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,30 @@ output "vpc_id" {
   value       = module.vpc.default_vpc_id
 }
 
+output "project_name" {
+  description = "Project Name"
+  value       = var.resource_tags["project"]
+}
+
+output "environment" {
+  description = "Project environment (Dev/QA/Staging/Prod)"
+  value       = var.resource_tags["environment"]
+}
+
+output "vpc_cidr_block" {
+  description = "VPC CIDR block"
+  value       = var.vpc_cidr_block
+}
+
+# Print Vpc Public Subnets
+output "vpc_public_subnets" {
+  description = "Vpc Public Subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "resource_tags" {
+  description = "Resource Tags"
+  value       = var.resource_tags
+}
+
+
